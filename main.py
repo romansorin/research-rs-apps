7c716999a0c9701de37d61337e860a3d987cac95
Roman Sorin <rmaximsorin@gmail.com>
2019-12-09 11:29:09 -0500

    iterate over list of sites & save screenshots

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from selenium import webdriver
 
-url = 'https://stripe.com'
-path = './scrape.png'
+sites = [
+    {
+        'name': 'stripe',
+        'url': 'https://stripe.com'},
+    {
+        'name': 'romansorin',
+        'url': 'https://romansorin.com'
+    },
+    {
+        'name': 'status-romansorin',
+        'url': 'https://status.romansorin.com'
+    }
+]
 
 geckodriver = './geckodriver'
 
 options = webdriver.FirefoxOptions()
 options.headless = True
-
 driver = webdriver.Firefox(executable_path=geckodriver, options=options)
-driver.get(url)
-el = driver.find_element_by_tag_name('body')
-el.screenshot(path)
+
+for site in sites:
+    print(site['name'])
+    path = f"./screenshots/{site['name']}.png"
+    driver.get(site['url'])
+    el = driver.find_element_by_tag_name('body')
+    el.screenshot(path)
+
 driver.quit()
