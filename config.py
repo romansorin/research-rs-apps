776815413572e73e09294f1efa572465a0cbe7dc
Roman Sorin <rmaximsorin@gmail.com>
2019-12-10 12:16:59 -0500

    set profile for caching

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 
 geckodriver = './geckodriver'
 
+profile = webdriver.FirefoxProfile('./profile')
+
 options = webdriver.FirefoxOptions()
 options.headless = True
-driver = webdriver.Firefox(executable_path=geckodriver, options=options)
+driver = webdriver.Firefox(executable_path=geckodriver,
+                           options=options, firefox_profile=profile)
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from config import driver
 from sites import sites
 
 
-driver.implicitly_wait(90)
+driver.implicitly_wait(60)
 
 for site in sites:
+    print(site)
     driver.get(site['url'])
     page = driver.find_element_by_tag_name('body')
 
