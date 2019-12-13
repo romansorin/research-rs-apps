d12bbeb33096acec3b02b9b90d7b25fa16109d08
Roman Sorin <rmaximsorin@gmail.com>
2019-12-13 08:14:34 -0500

    comment

--- topsites/script.py
+++ topsites/script.py
@@ -3,6 +3,7 @@ lower = 1
 interval = 100
 sets = []
 
+# Find ranges given limit and interval
 for i in range(lower, limit, interval):
     intervals = [i, i + interval - 1]
     print(f"/api?Action=TopSites&Count={interval}&CountryCode=US&ResponseGroup=Country&Output=json&Start={intervals[0]}")
