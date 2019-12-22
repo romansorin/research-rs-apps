36b0445b91c4f8308dc2ce8267caa78d68805989
Roman Sorin <rmaximsorin@gmail.com>
2019-12-22 08:41:26 -0500

    status message

--- screenshot/main.py
+++ screenshot/main.py
@@ -2,10 +2,12 @@ from sites import sites
 from config import driver
 
 for site in sites:
+    print("Beginning site: ", site["name"])
     driver.get(site["url"])
     page = driver.find_element_by_tag_name("body")
     filename = site["name"]
     path = f"./screenshots/{filename}.png"
     page.screenshot(path)
+    print("Finished site")
 
 driver.quit()
