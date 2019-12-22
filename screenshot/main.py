8cae3cab688c7667de1b7b8c1fa78212635a08a7
Roman Sorin <roman@romansorin.com>
2019-12-22 08:35:27 -0500

    Added windows firefox binary

--- screenshot/main.py
+++ screenshot/main.py
@@ -1,11 +1,10 @@
-from .config import driver
-from .sites import sites
-
+from sites import sites
+from config import driver
 
 for site in sites:
-    driver.get(site['url'])
-    page = driver.find_element_by_tag_name('body')
-    filename = site['name']
+    driver.get(site["url"])
+    page = driver.find_element_by_tag_name("body")
+    filename = site["name"]
     path = f"./screenshots/{filename}.png"
     page.screenshot(path)
 
--- screenshot/sites.py
+++ screenshot/sites.py
@@ -1,33 +1,121 @@
 sites = [
     {
-        'name': 'stripe',
-        'url': 'https://stripe.com'},
-    {
-        'name': 'romansorin',
-        'url': 'https://romansorin.com'
+        'name': 'lewagon',
+        'url': 'https://lewagon.com'
     },
     {
-        'name': 'status-romansorin',
-        'url': 'https://status.romansorin.com'
+        'name': 'evercheck',
+        'url': 'https://evercheck.com'
     },
     {
-        'name': 'airbnb',
-        'url': 'https://airbnb.com'
+        'name': 'evolt',
+        'url': 'https://evolt.io'
     },
     {
-        'name': 'twitter',
-        'url': 'https://twitter.com'
+        'name': 'uscreen-leadzen',
+        'url': 'https://uscreen.tv/leadzen'
     },
     {
-        'name': 'reddit',
-        'url': 'https://reddit.com'
+        'name': 'generated',
+        'url': 'https://generated.photos'
     },
     {
-        'name': 'tumblr',
-        'url': 'https://tumblr.com'
+        'name': 'quokka',
+        'url': 'https://quokka.io'
     },
     {
-        'name': 'google',
-        'url': 'https://google.com'
+        'name': 'supercast',
+        'url': 'https://supercast.com'
+    },
+    {
+        'name': 'upcut',
+        'url': 'https://upcutstudio.com'
+    },
+    {
+        'name': 'liramail',
+        'url': 'https://liramail.com'
+    },
+    {
+        'name': 'webflow',
+        'url': 'https://webflow.com'
+    },
+    {
+        'name': 'envoyer',
+        'url': 'https://envoyer.io'
+    },
+    {
+        'name': 'sizzy',
+        'url': 'https://sizzy.co'
+    },
+    {
+        'name': 'forge',
+        'url': 'https://forge.laravel.com'
+    },
+    {
+        'name': 'vapor',
+        'url': 'https://vapor.laravel.com'
+    },
+    {
+        'name': 'nova',
+        'url': 'https://nova.laravel.com'
+    },
+    {
+        'name': 'todoist',
+        'url': 'https://todoist.com'
+    },
+    {
+        'name': 'wire',
+        'url': 'https://wire.com'
+    },
+    {
+        'name': 'wake',
+        'url': 'https://wake.com'
+    },
+    {
+        'name': 'glyph',
+        'url': 'https://glyph.pro'
+    },
+    {
+        'name': 'stripe-connect',
+        'url': 'https://stripe.com/connect'
+    },
+    {
+        'name': 'slack',
+        'url': 'https://slack.com'
+    },
+    {
+        'name': 'makeitinua',
+        'url': 'https://makeitinua.com'
+    },    {
+        'name': 'pitch',
+        'url': 'https://pitch.com'
+    },
+    {
+        'name': 'designcode',
+        'url': 'https://designcode.io'
+    },
+    {
+        'name': 'atlassian',
+        'url': 'https://atlassian.com'
+    },
+    {
+        'name': 'forestadmin',
+        'url': 'https://forestadmin.com'
+    },
+    {
+        'name': 'lattice',
+        'url': 'https://lattice.com'
+    },
+    {
+        'name': 'stripe-atlas',
+        'url': 'https://stripe.com/atlas'
+    },
+    {
+        'name': 'evrybo',
+        'url': 'https://evrybo.com'
+    },
+    {
+        'name': 'lobe',
+        'url': 'https://lobe.ai'
     }
 ]
