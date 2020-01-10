df73ca21722bec0b8dec38abc709e80f2eac6f75
Roman Sorin <rmaximsorin@gmail.com>
2020-01-10 12:55:01 -0500

    Moved geckodriver executables into drivers folder

--- screenshot/config.py
+++ screenshot/config.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 # Path to geckodriver (firefox) executable
-geckodriver = "./geckodriver"
+geckodriver = "./drivers/geckodriver"
 
 # Configure Firefox driver settings
 profile = webdriver.FirefoxProfile("./profile")
@@ -13,5 +13,4 @@ options.add_argument("--height=1440")
 driver = webdriver.Firefox(
     executable_path=geckodriver, options=options, firefox_profile=profile
 )
-
 driver.implicitly_wait(60)
