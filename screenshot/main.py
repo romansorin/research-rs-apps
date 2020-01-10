5c836afd9df1d2b2cdf2f5683c2e2aecef34205e
Roman Sorin <rmaximsorin@gmail.com>
2020-01-10 12:50:50 -0500

    Fixed bug #1

--- screenshot/main.py
+++ screenshot/main.py
@@ -3,7 +3,8 @@ from config import driver
 import time
 from datetime import datetime
 
-SCROLL_PAUSE_TIME = 0.5
+SCROLL_PAUSE_TIME = 5
+RESCROLL_PAUSE_TIME = 0.5
 
 # Get scroll height
 
@@ -31,7 +32,7 @@ for site in sites:
     while current_scroll < last_height:
         print(f"Rescrolling page to {current_scroll}")
         driver.execute_script(f"window.scrollTo(0, {current_scroll})")
-        time.sleep(SCROLL_PAUSE_TIME)
+        time.sleep(RESCROLL_PAUSE_TIME)
         current_scroll += 200
 
     filename = site["name"]
@@ -39,6 +40,7 @@ for site in sites:
     print(driver.get_window_size())
     driver.set_window_size(2560, last_height + 150)
     driver.set_window_position(0, 0)
+    driver.execute_script("window.scrollTo(0, 0)")
     print(driver.get_window_size())
     driver.find_element_by_tag_name("body").screenshot(path)
     time_elapsed = datetime.now() - startTime
