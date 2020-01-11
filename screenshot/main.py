7f40608080589c7f9c171c65d27924632ddeec9b
Roman Sorin <rmaximsorin@gmail.com>
2020-01-10 19:03:21 -0500

    more refactoring

--- screenshot/main.py
+++ screenshot/main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 SCROLL_PAUSE_TIME = 5
 RESCROLL_PAUSE_TIME = 0.5
+RESCROLL_INCREMENTS = 200
 
 
 def now():
@@ -19,13 +20,25 @@ def get_scroll_height():
     return driver.execute_script("return document.body.scrollHeight")
 
 
+def set_window_height(height=1440):
+    return driver.set_window_size(2560, height)
+
+
+def get_window_height():
+    return driver.get_window_size()
+
+
+def scroll_to(height):
+    driver.execute_script(f"window.scrollTo(0, {height}")
+
+
 def scroll(height):
     while True:
         if LOGGING: print(f"Scrolling to height {height}")
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        scroll_to("document.body.scrollHeight")
         time.sleep(SCROLL_PAUSE_TIME)
 
-        new_height = driver.execute_script("return document.body.scrollHeight")
+        new_height = get_scroll_height()
         if new_height == height:
             break
         height = new_height
@@ -34,37 +47,34 @@ def scroll(height):
 
 def rescroll(height):
     current_scroll = 0
+
     while current_scroll < height:
         if LOGGING: print(f"Rescrolling page to {current_scroll}")
-        driver.execute_script(f"window.scrollTo(0, {current_scroll})")
+        scroll_to(current_scroll)
         time.sleep(RESCROLL_PAUSE_TIME)
-        current_scroll += 200
+        current_scroll += RESCROLL_INCREMENTS
 
 
-def screenshot(filename):
-    if LOGGING: print(driver.get_window_size())
-
-    driver.set_window_size(2560, last_height + 150)
+def screenshot(filename, height):
+    if LOGGING: print(get_window_height())
+    set_window_height(height + 150)
     driver.set_window_position(0, 0)
-    driver.execute_script("window.scrollTo(0, 0)")
+    if LOGGING: print(get_window_height())
 
-    if LOGGING: print(driver.get_window_size())
-
+    scroll_to("document.body.scrollHeight")
     path = f"./screenshots/{filename}.png"
-
     driver.find_element_by_tag_name("body").screenshot(path)
+
     if LOGGING: print(f"Finished site in {time_elapsed(start_time, datetime.now())}")
 
 
 def setup(name, url):
     if LOGGING: print(f"Beginning site: {name}")
 
-    driver.set_window_size(2560, 1440)
+    set_window_height()
     driver.get(url)
 
     return now(), get_scroll_height()
-
-
 
 
 if __name__ == "__main__":
@@ -72,5 +82,5 @@ if __name__ == "__main__":
         start_time, last_height = setup(site["name"], site["url"])
         last_height = scroll(last_height)
         rescroll(last_height)
-        screenshot(site["name"])
+        screenshot(site["name"], last_height)
     driver.quit()
