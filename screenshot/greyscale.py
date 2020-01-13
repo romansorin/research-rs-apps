cb7a605d05cf183156479db7503922fa4b5dbe17
Roman Sorin <rmaximsorin@gmail.com>
2020-01-13 10:03:37 -0500

    Moved into function

--- screenshot/greyscale.py
+++ screenshot/greyscale.py
@@ -1,14 +1,21 @@
 import cv2
 import os
 
+SCREENSHOT_PATH = 'screenshots'
+SCREENSHOT_GREY_PATH = f'{SCREENSHOT_PATH}/greyscale'
+
+
 def convert_to_greyscale(filename):
     print("Reading: ", filename)
-    image = cv2.imread(fr'screenshots/{filename}')
+    image = cv2.imread(f'{SCREENSHOT_PATH}/{filename}')
     grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    status = cv2.imwrite(fr'screenshots/greyscale/{filename}', grey)
+    status = cv2.imwrite(f'{SCREENSHOT_GREY_PATH}/{filename}', grey)
     print("Wrote greyscale image to directory: ", status)
 
 
-for file in os.listdir('screenshots'):
-    if file.endswith('.png'):
-        convert_to_greyscale(file)
+def convert_all():
+    for file in os.listdir('screenshots'):
+        if file.endswith('.png'):
+            convert_to_greyscale(file)
+
+convert_all()
