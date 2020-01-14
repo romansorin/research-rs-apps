e912c3e6106ea0e187c8993a26c0d2e10fe29a67
Roman Sorin <rmaximsorin@gmail.com>
2020-01-14 12:15:18 -0500

    submodules

--- topsites/script.py
+++
@@ -1,9 +0,0 @@
-limit = 10001
-lower = 1
-interval = 100
-sets = []
-
-# Find ranges given limit and interval
-for i in range(lower, limit, interval):
-    intervals = [i, i + interval - 1]
-    print(f"/api?Action=TopSites&Count={interval}&CountryCode=US&ResponseGroup=Country&Output=json&Start={intervals[0]}")
